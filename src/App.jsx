b02d8b5a3c514ae7ffd2559e372723d37fac5695
Cailum Leyshon <107961224+cailumleyshon@users.noreply.github.com>
2023-11-21 20:40:56 +0000

    hero structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import Navbar from "./components/nav/Navbar";
+import Hero from "./components/hero/Hero";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
     </div>
   );
 }
