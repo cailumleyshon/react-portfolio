7d7fa97ee555a641eab3f67ec5c8298dba9096ea
Cailum Leyshon <107961224+cailumleyshon@users.noreply.github.com>
2023-11-22 14:38:45 +0000

    about structure and styling and other bits

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,6 +2,7 @@ import { getImageUrl } from "../../utils";
 import styles from "./Hero.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/fontawesome-free-brands";
 
 export default function Hero() {
   return (
@@ -15,15 +16,39 @@ export default function Hero() {
         <h1 className={styles.title}>
           Hi, I'm <span className={styles.name}>Cailum</span>
         </h1>
-        <p className={styles.bio}>I'm a full-stack developer ...</p>
-        <a className={styles.contact} href="mailto:cailumleyshon@gmail.com">
-          Email me
+        <p className={styles.bio}>
+          I'm a Junior full-stack developer with an{" "}
+          <span className={styles.eyeBlink}>eye</span> for design and a passion
+          for creating interesting web applications that bridge the gap between
+          functionality and aesthetics.
+        </p>
+        <div className={styles.links}>
           <FontAwesomeIcon
-            className={styles.icon}
+            onClick={() =>
+              (window.location.href = "mailto:cailumleyshon@gmail.com")
+            }
+            className={styles.icon + " " + styles.emailIcon}
             icon={faEnvelopeOpen}
             style={{ color: "#d1610b" }}
           />
-        </a>
+          <FontAwesomeIcon
+            onClick={() =>
+              (window.location.href =
+                "https://www.linkedin.com/in/cailum-leyshon-05965923b")
+            }
+            className={styles.icon}
+            icon={faLinkedin}
+            style={{ color: "#d1610b" }}
+          />
+          <FontAwesomeIcon
+            onClick={() =>
+              (window.location.href = "https://github.com/cailumleyshon")
+            }
+            className={styles.icon}
+            icon={faGithub}
+            style={{ color: "#d1610b" }}
+          />
+        </div>
       </div>
       <div className={styles.top}></div>
       <div className={styles.bottom}></div>
